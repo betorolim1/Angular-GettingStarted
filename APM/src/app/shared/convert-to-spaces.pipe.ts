e40e8b573ec826137bc3dbc5d76da094513386a9
Adalberto Rolim Nogueira <adalberto.nogueira@autotrac.com.br>
2020-01-10 18:02:26 -0300

    Continua

---
+++ APM/src/app/shared/convert-to-spaces.pipe.ts
@@ -0,0 +1,10 @@
+import { PipeTransform, Pipe } from "@angular/core";
+
+@Pipe({
+  name: "convertToSpaces"
+})
+export class ConvertToSpacesPipe implements PipeTransform {
+  transform(value: string, caracter: string): string {
+    return value.replace(caracter, " ");
+  }
+}
