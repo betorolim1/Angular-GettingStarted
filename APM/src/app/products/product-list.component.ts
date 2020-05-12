260c357622a1fad36626e838bf4b7c58cf74c328
Adalberto Rolim Nogueira <adalberto.nogueira@autotrac.com.br>
2020-05-12 13:46:26 -0300

    Cont

--- APM/src/app/products/product-list.component.ts
+++ APM/src/app/products/product-list.component.ts
@@ -1,22 +1,26 @@
 import { Component, OnInit } from "@angular/core";
 import { IProduct } from "./product";
+import { ProductService } from "./product.service";
 
 @Component({
-  selector: "pm-products",
   templateUrl: "./product-list.component.html",
   styleUrls: ["./product-list.component.css"]
 })
 export class ProductListComponent implements OnInit {
-  /**
-   *
-   */
-  constructor() {
-    this.filteredProducts = this.products;
-    this.listFilter = "cart";
-  }
+  filteredProducts: IProduct[];
+  products: IProduct[] = [];
+  errorMessage: string;
+
+  constructor(private productService: ProductService) {}
 
   ngOnInit(): void {
-    console.log("On Init!!");
+    this.productService.getProducts().subscribe({
+      next: products => {
+        this.products = products;
+        this.filteredProducts = this.products;
+      },
+      error: err => (this.errorMessage = err)
+    });
   }
 
   pageTitle: string = "Product List";
@@ -35,30 +39,6 @@ export class ProductListComponent implements OnInit {
       : this.products;
   }
 
-  filteredProducts: IProduct[];
-  products: IProduct[] = [
-    {
-      productId: 1,
-      productName: "Leaf Rake",
-      productCode: "GDN-0011",
-      releaseDate: "March 19, 2019",
-      description: "Leaf rake with 48-inch wooden handle.",
-      price: 19.95,
-      starRating: 3.2,
-      imageUrl: "assets/images/leaf_rake.png"
-    },
-    {
-      productId: 2,
-      productName: "Garden Cart",
-      productCode: "GDN-0023",
-      releaseDate: "March 18, 2019",
-      description: "15 gallon capacity rolling garden cart",
-      price: 32.99,
-      starRating: 4.2,
-      imageUrl: "assets/images/garden_cart.png"
-    }
-  ];
-
   performFilter(filterBy: string): IProduct[] {
     filterBy = filterBy.toLocaleLowerCase();
     return this.products.filter(
@@ -66,8 +46,11 @@ export class ProductListComponent implements OnInit {
         product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1
     );
   }
-
   toggleImage(): void {
     this.showImage = !this.showImage;
+  }
+
+  onRatingClicked(message: string): void {
+    this.pageTitle = "Product List: " + message;
   }
 }
